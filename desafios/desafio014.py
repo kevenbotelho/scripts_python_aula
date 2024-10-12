1edfd25290a25c09123c35289dafd6771c25e156
kevenbotelho <kral7970l@gmail.com>
2024-10-12 17:07:29 -0300

    atualização

---
+++ desafios/desafio014.py
@@ -0,0 +1,2 @@
+n = float(input("Qual a temperatura em ºC: "))
+print(f"A temperatura de {n} corresponde a {(n * 9/5) + 32}ºF!")
---
+++ desafios/desafio016.py
@@ -0,0 +1,4 @@
+dias = int(input("Quantos dias alugados? "))
+km = float(input("Quantos Km rodados? "))
+pagar = (dias*60)+(km*0.15)
+print(pagar)
